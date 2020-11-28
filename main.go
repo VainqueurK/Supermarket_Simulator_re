e96bf71f78896ddcfc29badf5866bddbb5437316
Aine <ainemreynolds19@gmail.com>
2020-11-28 17:16:30 +0000

    Fixed open and close tills, changed timings

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"fmt"
 	"math/rand"
 	"runtime"
-	"strconv"
+	//"strconv"
 	"time"
 )
 
@@ -23,9 +23,11 @@ const (
 	fastQueueMaxNumOfItems   = 20
 	maxNumOfTills            = 8
 	minNumOfTills            = 1
-	minCashierSpeed          = 1
-	maxCashierSpeed          = 5
+	minCashierSpeed          = 1.0
+	maxCashierSpeed          = 2.0
 	queueLength              = 6
+	timeRunning 			 = 30
+	millisecPerRealHour 	 = float64((float64(timeRunning)/ 12) * 1000)
 )
 
 //Days of the week variables
@@ -46,22 +48,18 @@ var tills []till
 var hasFastTill bool
 var daysOfTheWeek = [...]string{"a", "b", "c", "d", "e", "f", "g"}
 
-//we're gonna have to put this array behind a mutex lock at some point
 var customers []customer
 var lastCustomerGenerated = time.Now()
+var lastTillChanged = time.Now()
 
-//we'll use this in the future
 var clock = time.Now()
 var totalCustomers = 0
 var customerCount = 0
 var currentNumOfCustomers = 0
 var numOfCutomersInShop = 0
-var numOfPeopleInQueue = 0
 var numOfOpenTills = 0
 var customersLostDueToImpatients = 0
 var running = true
-var millisecPerRealHour float64 = 0
-var timeRunning = 0
 var day float64 = 0
 var avgWaitTime float64 = 0
 
@@ -81,7 +79,7 @@ type customer struct {
 }
 
 type cashier struct {
-	scanSpeed int
+	scanSpeed float64
 }
 
 type till struct {
@@ -92,6 +90,7 @@ type till struct {
 	open bool
 	scannedItems  int
 	tillUsage     int
+	lastUsed	time.Time
 }
 
 type manager struct{}
@@ -108,8 +107,8 @@ func (a *automatic) RunSimulator() {
 	manager := manager{}
 	manager.GenerateTills()
 	//determine initial generation rate
-	twoMinInMilli := (millisecPerRealHour/60) * 2 
-	a.generationRate = float64((float64(numOfOpenTills) * twoMinInMilli))
+	twoMinInMilli := (millisecPerRealHour/60) * 2
+	a.generationRate = float64(twoMinInMilli)
 	a.generationRate = a.generationRate * day 
 	//a.generationRate = a.generationRate * thirtySecInMilli
 	//create two goroutines that will continuously generate customers and try to add them to a queue
@@ -122,7 +121,9 @@ func (a *automatic) RunSimulator() {
 	}
 
 	//Create a go rountine that consistently checks if tills should be opened or closed depending on the number of people in the supermarket
+	time.Sleep(time.Duration(20) * time.Millisecond)
 	go a.OpenTillIfBusy()
+	go a.CloseTills()
 	//runtime
 	time.Sleep(time.Duration(timeRunning) * time.Second)
 }
@@ -171,66 +172,30 @@ func getInputs() {
 			fmt.Println("Error: Invalid Input Detected")
 			fmt.Println("Example Usage: Enter a for Monday")
 		}
-	}
-
-	valid = false
-	//Runs for loop until there is a valid input for the runtime
-	for !valid {
-		fmt.Println("Enter the number of seconds you want the program to run for: ")
-		fmt.Scanln(&input)
-		//Checks to see if the input is an int
-		_, err := strconv.ParseInt(input, 10, 64)
-		if bool(err == nil) == true {
-			//If the input is an int it will convert the string to an int and end the for loop
-			if i, hmm := strconv.Atoi(input); hmm == nil {
-				valid = true
-				timeRunning = int(i)
-			}
-		} else {
-			fmt.Println("Error: Invalid Input Detected")
-			fmt.Println("Input should be in the form of an integer")
-		}
-	}
-	// make the time inputted by the user relevant to a 12 hour working day in the supermarket
-	millisecPerRealHour = float64((float64(timeRunning)/ 12) * 1000)
+	}	
 }
 
 func (m *manager) GenerateTills() {
 	//generate random number for the num of tills
-	numOfTills := randomNumberInclusive(minNumOfTills, maxNumOfTills)
+	numOfTills := int(randomNumberInclusive(2, maxNumOfTills))
 	tills = make([]till, maxNumOfTills)
 
 	index := 0
-	//guarantee fast till is generated if numOfTills > 1, and that if numOfTills == 1 it's a regular till
-	if numOfTills > 1 {
-		//guaranteed fast till
-		tills[0] = till{name: 1}
-		tills[0].SetUpTill(true)
-		index++
-		//guaranteed regular till
-		tills[1] = till{name: 2}
-		tills[1].SetUpTill(false)
-		index++
-		hasFastTill = true
-	} else {
-		//guaranteed regular till
-		tills[0] = till{name: 1}
-		tills[0].SetUpTill(false)
-		index++
-		hasFastTill = false
-	}
+	//guaranteed fast till
+	tills[0] = till{name: 1}
+	tills[0].SetUpTill(true)
+	index++
+	//guaranteed regular till
+	tills[1] = till{name: 2}
+	tills[1].SetUpTill(false)
+	index++
+	hasFastTill = true
 
-	maxItemsTill := 1
-	//generate the rest of the tills randomly
+	//maxItemsTill := 1
+	//generate the rest of the tills as regular tills
 	for i := index; i < maxNumOfTills; i++ {
 		tills[i] = till{name: (i + 1)}
-		//randomly decide if the till has a max number of items
-		maxItemsTill = randomNumberInclusive(1, 100)
-		if maxItemsTill > 10 {
-			tills[i].SetUpTill(false)
-		} else {
-			tills[i].SetUpTill(true)
-		}
+		tills[i].SetUpTill(false)
 	}
 
 	for i := 0; i < numOfTills; i++ {
@@ -255,9 +220,9 @@ func (t *till) SetUpTill(maxItemsTill bool) {
 
 func (a *automatic) GenerateCustomers() {
 	for running {
-		time.Sleep(10 * time.Millisecond)
 		//if a certain amount of time has passed since the last customer was generated generate a new customer
 		if time.Now().Sub(lastCustomerGenerated) > (time.Millisecond * time.Duration(a.generationRate)) {
+			//fmt.Printf("Time now - last customer generated = %v\n",time.Now().Sub(lastCustomerGenerated))
 			//generate customer
 			var patient bool
 			patientInt := randomNumberInclusive(1,3)
@@ -267,7 +232,7 @@ func (a *automatic) GenerateCustomers() {
 				patient = false
 			}
 
-			customer := customer{randomNumberInclusive(1, 200), patient, time.Now() , 0.0}
+			customer := customer{int(randomNumberInclusive(1, 200)), patient, time.Now() , 0.0}
 			//fmt.Printf("Customer patient attribute: %t\n", customer.patient)
 			//add to customer array
 			customers = append(customers, customer)
@@ -283,8 +248,8 @@ func (a *automatic) LookForSpaceInQueue() {
 	var index int
 	for running {
 		//Check for space every minute
-		oneMinInMilli := (millisecPerRealHour/60)
-		time.Sleep(time.Duration(oneMinInMilli) * time.Millisecond)
+		fiveSecInMilli := ((millisecPerRealHour/60)/60) * 5
+		time.Sleep(time.Duration(fiveSecInMilli) * time.Millisecond)
 		//check if customers are waiting
 		if len(customers) > 0 {
 			customer := customers[0]
@@ -307,9 +272,11 @@ func (a *automatic) LookForSpaceInQueue() {
 				if len(tills[index].queue) > 3   && customer.patient == false {
 					customersLostDueToImpatients++
 					numOfCutomersInShop--
+					customers = customers[1:]
+					currentNumOfCustomers--
 					continue
-				}
-				if tills[index].AddCustomerToQueue(customer) {
+				}else{
+					tills[index].AddCustomerToQueue(customer) 
 					customers = customers[1:]
 					currentNumOfCustomers--
 				}
@@ -319,22 +286,43 @@ func (a *automatic) LookForSpaceInQueue() {
 }
 
 func (a *automatic) OpenTillIfBusy(){
+	//check tills every 20 minutes  
+	twentyMinInMilli := (millisecPerRealHour/60) * 20
 	for running {
-		//check tills every 10minutes 
-		tenMinInMilli := (millisecPerRealHour/60) * 10
-		time.Sleep(time.Duration(tenMinInMilli) * time.Millisecond)
-		numOfPossibleCustomersForTills := numOfOpenTills * 7
-
-		if numOfOpenTills < 8 && numOfPossibleCustomersForTills < currentNumOfCustomers {
-			tills[numOfOpenTills].open = true
-			numOfOpenTills++
-			//fmt.Printf("Opening another Till num %d\n", tills[numOfOpenTills-1].name)
+		if time.Now().Sub(lastTillChanged) >  (time.Millisecond * time.Duration(twentyMinInMilli)){
+			lenOfQueues := 0
+			numTills := 0
+			for i:=0; i < numOfOpenTills; i++{
+				if tills[i].maxNumOfItems == 200{
+					lenOfQueues += len(tills[i].queue)
+					numTills++
+				}
+			}
+			lenOfQueues = lenOfQueues/numTills
+			if numOfOpenTills < 8 && lenOfQueues > 3 {
+				fmt.Printf("Time difference between now and last Till changed = %v\n", time.Now().Sub(lastTillChanged))
+				tills[numOfOpenTills].open = true
+				tills[numOfOpenTills].lastUsed = time.Now()
+				tills[numOfOpenTills].maxNumOfItems = 200
+				numOfOpenTills++
+				fmt.Printf("Opening another Till num %d\n", tills[numOfOpenTills-1].name)
+				lastTillChanged = time.Now()
+			}
 		}
+	}
+}
 
-		if numOfOpenTills > 1 && numOfPossibleCustomersForTills > currentNumOfCustomers{
-			tills[numOfOpenTills-1].open = false
-			numOfOpenTills--
-			//fmt.Printf("Closing a Till num %d\n", tills[numOfOpenTills].name)
+func (a *automatic) CloseTills(){
+	for running {	
+		twentyMinInMilli := (millisecPerRealHour/60) * 20
+		fortyMinInMilli := (millisecPerRealHour/60) * 40
+		if time.Now().Sub(lastTillChanged) >  (time.Millisecond * time.Duration(fortyMinInMilli)) {
+			if numOfOpenTills > 2 &&  time.Now().Sub(tills[numOfOpenTills-1].lastUsed) > (time.Millisecond * time.Duration(twentyMinInMilli)){
+				tills[numOfOpenTills-1].open = false
+				numOfOpenTills--
+				fmt.Printf("Closing a Till num %d\n", tills[numOfOpenTills].name)
+				lastTillChanged = time.Now()
+			}
 		}
 	}
 }
@@ -367,13 +355,28 @@ func shortestFastQueue() int {
 func (t *till) SendCustomerToCashier() {
 	for running {
 		//a wait time so the loop doesn't run too fast
-		time.Sleep(30 * time.Millisecond)
+		tensec := (((millisecPerRealHour)/60)/60) * 10
+		time.Sleep(time.Duration(tensec) * time.Millisecond)
 		//checks if queue is empty
 		if len(t.queue) == 0 {
 			//fmt.Println("queue empty")
 		} else {
+			
 			//removes customer from queue
 			currentCustomer := <-t.queue
+
+			customerCount++
+			endTime := time.Now()
+			waitT := float64(endTime.Sub(currentCustomer.stime).Milliseconds())
+			
+			// Get the actual customer wait time in relation to a day.
+			custWaitT := waitT * millisecPerRealHour
+			//fmt.Printf("Time running = %d waitT = %f milliPerSec = %f\n", timeRunning,waitT, milliPerRealSec)
+			//Get each customer wait time in minutes and assign it to the customer
+			currentCustomer.waitTime = (custWaitT / 1000)/60
+			//Add up all the wait times to use to get the average
+			avgWaitTime = float64(avgWaitTime) + currentCustomer.waitTime
+			fmt.Printf("Customer num %d wait time = %f minutes in till: %d\n", customerCount, currentCustomer.waitTime, t.name)
 			//fmt.Printf("Scanning %d items in Till %d\n", currentCustomer.numOfItems, t.name)
 			//call a method for the cashier to start scanning items
 			t.employee.ScanItems(currentCustomer)
@@ -392,29 +395,15 @@ func (t *till) AddCustomerToQueue(c customer) bool {
 
 		//adds customer to queue
 		t.queue <- c
-		numOfPeopleInQueue++
+		t.lastUsed = time.Now()
 		return true
 	}
 }
 
 func (c *cashier) ScanItems(customer customer) {
-	customerCount++
-	scanTime := customer.numOfItems * c.scanSpeed
+	threeSec := (((millisecPerRealHour)/60)/60) * 3
+	scanTime := float64(customer.numOfItems) * c.scanSpeed * threeSec
 	time.Sleep(time.Duration(scanTime) * time.Millisecond)
-
-	//calculate the wait time by getting the difference between their start time and the current time
-	endTime := time.Now()
-	waitT := float64(endTime.Sub(customer.stime).Milliseconds())
-	
-	// Get the actual customer wait time in relation to a day.
-	custWaitT := waitT * millisecPerRealHour
-	//fmt.Printf("Time running = %d waitT = %f milliPerSec = %f\n", timeRunning,waitT, milliPerRealSec)
-	//Get each customer wait time in minutes and assign it to the customer
-	customer.waitTime = (custWaitT / 1000)/60
-	//Add up all the wait times to use to get the average
-	avgWaitTime = float64(avgWaitTime) + customer.waitTime
-	fmt.Printf("Customer num %d wait time = %f minutes\n", customerCount, customer.waitTime)
-	numOfPeopleInQueue--
 	numOfCutomersInShop--
 }
 
@@ -436,14 +425,15 @@ func main() {
 	fmt.Printf("Total number of tills open: %d\n", numOfOpenTills)
 	fmt.Printf("Total number of impatient customers lost: %d\n", customersLostDueToImpatients)
 	fmt.Printf("Average customer wait time: %d minutes\n", (int(avgWaitTime)/customerCount))
+	fmt.Printf("Running time = %d Milliseconds in an hour = %f\n", timeRunning, millisecPerRealHour)
 	for i := 0; i < len(tills); i++ {
 		fmt.Printf("Number of items scanned by till %d: %d\n", tills[i].name, tills[i].scannedItems)
 		fmt.Printf("Number of customers processed by till %d: %d\n", tills[i].name, tills[i].tillUsage)
 	}
 }
 
-func randomNumberInclusive(min, max float64) int {
+func randomNumberInclusive(min, max float64) float64 {
 	num := min + rand.Float64()*(max-min)
 	//fmt.Println(num, int(num))
-	return int(num)
+	return float64(num)
 }
